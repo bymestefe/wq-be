fa4fb745c103a962909cd04b61f54cec076ca352
bymestefe <mestefesahbaz34@gmail.com>
2024-03-11 23:19:08 +0300

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./src/routes/v1');
+const helmet = require('helmet')
+const cors = require('cors')
 
 
 const app = express();
@@ -8,6 +10,8 @@ const port = process.env.PORT || 1453;
 
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
 app.use('/api/v1', routes);
 
 app.get('/', (req, res) => {
