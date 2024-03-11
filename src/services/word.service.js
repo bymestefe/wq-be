1a35f866ff6746df22a3164121812baef0715ab1
bymestefe <mestefesahbaz34@gmail.com>
2024-03-11 22:55:10 +0300

    edited

--- src/services/word.service.js
+++ src/services/word.service.js
@@ -4,14 +4,11 @@ const StringHelper = require('../utils/str');
 
 const getWordsWithLetters = async () => {
     const result = await WordRepository.getWordsWithLetters();
-    let id = 1;
 
     result.forEach(res => {
-        res.id = id;
         res.letter = StringHelper.capitalizeFirstLetter(res.letter);
         res.answer = StringHelper.capitalizeFirstLetter(res.answer);
         res.question = StringHelper.capitalizeFirstLetter(res.question);
-        id++;
 
         delete res.startswith;
         delete res.rn;
