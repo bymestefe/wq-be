0a369edd6083828941891efd416cfa06a20f1654
bymestefe <mestefesahbaz34@gmail.com>
2024-03-11 21:41:29 +0300

    added new method

--- src/repositories/word.repo.js
+++ src/repositories/word.repo.js
@@ -12,17 +12,44 @@ const WordRepository = {
             SELECT l.letter, w.*
             FROM Letters l
             LEFT JOIN (
-                SELECT id, word as answer, definition as question, startswith,
+                SELECT id, word as answer, definition as question, startswith, type,
                     ROW_NUMBER() OVER (PARTITION BY startswith ORDER BY RANDOM()) AS rn
                 FROM words
             ) w ON l.letter = w.startswith AND w.rn = 1;
         `;
         try {
             const result = await Word.sequelize.query(query, { type: Word.sequelize.QueryTypes.SELECT });
+            for (let i = 0; i < result.length; i++) {
+                if(result[i].question.length > 125){
+                    while (result[i].question.length > 125) {
+                        result[i] = await WordRepository.getWordByStartsWith(result[i].startswith)
+                    }
+                }
+            }
             return result;
         } catch (error) {
             console.log(error);
             throw new Error('Failed to get words with letters');
+        }
+    },
+
+    getWordByStartsWith: async (startsWith) => {
+        const query = `
+            SELECT id, word as answer, definition as question, startswith as letter, type
+            FROM words
+            WHERE startswith = :startsWith
+            ORDER BY RANDOM()
+            LIMIT 1;
+        `;
+        try {
+            const result = await Word.sequelize.query(query, { 
+                type: Word.sequelize.QueryTypes.SELECT,
+                replacements: { startsWith }
+            });
+            return result[0];
+        } catch (error) {
+            console.log(error);
+            throw new Error('Failed to get word by startsWith');
         }
     }
 }
--- src/services/word.service.js
+++ src/services/word.service.js
@@ -1,9 +1,25 @@
 const WordRepository = require('../repositories/word.repo');
+const { encrypt } = require('../utils/encryption');
+const StringHelper = require('../utils/str');
 
 const getWordsWithLetters = async () => {
     const result = await WordRepository.getWordsWithLetters();
-    return result;
+    let id = 1;
+
+    result.forEach(res => {
+        res.id = id;
+        res.letter = StringHelper.capitalizeFirstLetter(res.letter);
+        res.answer = StringHelper.capitalizeFirstLetter(res.answer);
+        res.question = StringHelper.capitalizeFirstLetter(res.question);
+        id++;
+
+        delete res.startswith;
+        delete res.rn;
+        
+    });
+    return encrypt(JSON.stringify(result));
 }
+
 
 module.exports = {
     getWordsWithLetters
