7dc3011765b94bedfaed0274396fcfd94dd5372e
bymestefe <mestefesahbaz34@gmail.com>
2024-03-11 23:35:25 +0300

    added new package

--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -1,13 +1,10 @@
-const crypto = require('crypto');
+const CryptoJS = require('crypto-js');
 
 const secretKey = process.env.SECRET_KEY;
 
 const encrypt = (text) => {
-    const key = Buffer.alloc(32, secretKey, 'utf-8');
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
+    const cipherText = CryptoJS.AES.encrypt(text, secretKey).toString();
+    return cipherText;
 }
 
 module.exports = {
